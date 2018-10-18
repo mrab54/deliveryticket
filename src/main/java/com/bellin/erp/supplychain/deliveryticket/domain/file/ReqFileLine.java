d0262ff713d6128af24bbaffdcd91c6406dbd2cd
Mike Rabas <mrab54@gmail.com>
2018-10-17 21:04:24 -0500

    WIP

--- src/main/java/com/bellin/erp/supplychain/deliveryticket/domain/file/ReqFileLine.java
+++ src/main/java/com/bellin/erp/supplychain/deliveryticket/domain/file/ReqFileLine.java
@@ -4,16 +4,21 @@ import java.util.Map;
 
 public class ReqFileLine {
     private int index;
+    private TreeMap<String, ReqFileLineField> reqFileLineFields = new TreeMap<String, ReqFileLineField>();
     //log
     //confObj
 
     //public ReqFileLine(int index, confObj){}
     public ReqFileLine(int index) {
         this.index = index;
+        // TODO
+        // for every lineField in the config, this.reqFileLineFields.put(<fieldName>, new ReqFileLineField(fieldName,...))
     }
 
     public void read(Map<String, String> lineMap) {
-
+        // // NOPE for k in lineMap
+        // for k,v in reqFileLineFields
+        //   v.read(lineMap)
     }
 
 }
---
+++ src/main/java/com/bellin/erp/supplychain/deliveryticket/lineitem/ReqFileLineItem.java
@@ -0,0 +1,17 @@
+package com.bellin.erp.supplychain.deliveryticket.lineitem;
+
+import java.util.Map;
+
+public class ReqFileLineItem {
+    //log
+    private String value;
+    private String name;
+
+    public ReqFileLineItem(String name) {
+        this.name = name;
+    }
+
+    public void read(Map<String, String> lineMap) {
+
+    }
+}
